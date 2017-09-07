0e70083a109a61cdc84123d2162eb099653c0181
ostryzhniuk <andrii.ostryzhniuk@gmail.com>
2017-09-07 19:29:32 +0300

    back end of login in

--- src/main/java/andrii/controllers/DefaultController.java
+++ src/main/java/andrii/controllers/DefaultController.java
@@ -1,12 +1,22 @@
 package andrii.controllers;
 
+import andrii.dto.LoginDTO;
 import andrii.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class DefaultController {
@@ -16,9 +26,23 @@ public class DefaultController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultController.class);
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @GetMapping("/currentUser")
     public User currentUser(){
         return userService.getCurrentUser();
+    }
+
+    @PostMapping("/authorize")
+    public User login(@RequestBody LoginDTO loginDTO, HttpServletRequest httpServletRequest) {
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(loginDTO.getEmail(), loginDTO.getPassword());
+        token.setDetails(new WebAuthenticationDetails(httpServletRequest));
+        Authentication authentication = authenticationManager.authenticate(token);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
 }
