b13175ed6722fec3cce81f457154a0222692b9be
ostryzhniuk <andrii.ostryzhniuk@gmail.com>
2017-09-07 17:43:38 +0300

    UserRoleDAO created

---
+++ src/main/java/andrii/dao/UserRoleDAO.java
@@ -0,0 +1,34 @@
+package andrii.dao;
+
+import andrii.entities.UserRole;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class UserRoleDAO extends GenericDAO<UserRole> {
+
+    @Override
+    public void save(UserRole userRole) {
+        getSession().save(userRole);
+    }
+
+    @Override
+    public List<UserRole> getObjects() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void update(UserRole userRole) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void delete(UserRole userRole) {
+        throw new UnsupportedOperationException();
+    }
+
+}
+
