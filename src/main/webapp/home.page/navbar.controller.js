c4e7c5e727e74db11d1e95931a7f8c9dca888c8a
ostryzhniuk <andrii.ostryzhniuk@gmail.com>
2017-09-08 18:17:55 +0300

    color styling

--- src/main/webapp/home.page/navbar.controller.js
+++ src/main/webapp/home.page/navbar.controller.js
@@ -4,6 +4,23 @@ angular
 .module('messengerApp')
 .controller('navbarCtrl', function ($http, $scope, $rootScope, $uibModal) {
 
+    $http.get('/currentUser').then(function(response) {
+        $rootScope.user = response.data;
+    });
+
+    $rootScope.isAuthority = function (role) {
+        if ($rootScope.user == undefined) {
+            return false;
+        }
+        var authorities = $rootScope.user.authorities;
+        for (var authority in authorities) {
+            if (authorities[authority].authority == role) {
+                return true;
+            }
+        }
+        return false;
+    };
+
     $scope.logout = function () {
         $http.get('/logout');
         window.location.replace('#!/home');
