fd92b293456f59b31e83ed7fc1ccc64507845ea0
ostryzhniuk <andrii.ostryzhniuk@gmail.com>
2017-09-08 17:07:02 +0300

    logout created

--- src/main/java/andrii/controllers/DefaultController.java
+++ src/main/java/andrii/controllers/DefaultController.java
@@ -6,18 +6,21 @@ import andrii.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class DefaultController {
@@ -49,6 +52,15 @@ public class DefaultController {
     @PostMapping("/signUp")
     public void signUp(@RequestBody UserSignUpDTO userDTO) {
         userService.save(userDTO);
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
     }
 
 }
--- src/main/webapp/home.page/navbar.controller.js
+++ src/main/webapp/home.page/navbar.controller.js
@@ -4,6 +4,9 @@ angular
 .module('messengerApp')
 .controller('navbarCtrl', function ($http, $scope, $rootScope, $uibModal) {
 
-
+    $scope.logout = function () {
+        $http.get('/logout');
+        window.location.replace('#!/home');
+    };
 
 });
