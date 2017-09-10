8239434dd43096ead381af85baa15998e41b1ee9
ostryzhniuk <andrii.ostryzhniuk@gmail.com>
2017-09-10 18:16:19 +0300

    checking if my profile

--- src/main/webapp/profile/profile.component.js
+++ src/main/webapp/profile/profile.component.js
@@ -6,12 +6,26 @@ angular.
 module('profile').
 component('profile', {
     templateUrl: '/profile/profile.template.html',
-    controller: ['$http', '$scope', '$routeParams',
-        function LoginController($http, $scope, $routeParams) {
+    controller: ['$http', '$scope', '$routeParams', '$rootScope',
+        function LoginController($http, $scope, $routeParams, $rootScope) {
 
             $http.get('/user/' + $routeParams.userId + '?loadImage=true').then(function(response) {
-                $scope.user = response.data;
+                $scope.profile = response.data;
             });
+
+            $http.get('/currentUser').then(function(response) {
+                $rootScope.user = response.data;
+            });
+
+            $scope.myProfile = function () {
+                if ($rootScope.user == undefined || $scope.profile == undefined) {
+                    return false;
+                }
+                if ($rootScope.user.id == $scope.profile.id) {
+                    return true;
+                }
+                return false;
+            };
 
         }
     ]
